24f2bccb8070058c12f6361ef55c56b9f7d8dfcd
GregDeveaux <deveauxgreg@gmail.com>
2023-02-07 20:03:03 +0100

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Greg-Mini on 07/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
